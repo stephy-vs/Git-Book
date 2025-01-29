b3bab58bbe47a329b86e162ae0232e07430a5251
stephy-vs <vsstephy.ptf@gmail.com>
2025-01-29 23:02:32 +0530

    child branch commit1

--- src/main/java/com/fileupload/Fileupload/entity/FileData.java
+++ src/main/java/com/fileupload/Fileupload/entity/FileData.java
@@ -9,21 +9,26 @@ import lombok.Data;
 @Table(name = "filetable")
 @Builder
 public class FileData {
+    //primaryKey
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id")
     private int id;
 
+    //user name
     @Column(name = "name")
     private String name;
 
+    //user type
     @Column(name = "type")
     private String type;
 
+    //profile photo
     @Lob
     @Column(name = "imagedata",length = 10000)
     private byte[] imagedata;
 
+    //userId foreign key
     @ManyToOne
     @JoinColumn(name = "uId")
     private UsersModel usersModel;
